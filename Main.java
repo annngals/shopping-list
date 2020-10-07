2ea4a52a80dcf73b06980b1fee8d581dd73697ef
annngals <57696371+annngals@users.noreply.github.com>
2020-10-07 10:29:31 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*ругается даже на сканнер не понимаю что происходит*/
+        public static void main(String[] args) throws IOException {
+            Scanner sc = new Scanner(new File("list.txt"));
+        }
+
+        TreeMap<String, Integer> products = new TreeMap();
+        TreeMap<String, TreeMap<String, Integer>> customers = new TreeMap();
+
+
+        for (String customer: customers.keySet()) {
+
+        }
+}
