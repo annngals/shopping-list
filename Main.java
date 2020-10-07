839c2eb9877b9b5043636e4b42e2909875f24a56
annngals <57696371+annngals@users.noreply.github.com>
2020-10-07 19:05:06 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,38 +6,42 @@ public class Main {
 
     public static void main(String[] args) throws IOException{
 
-        String name = "";
-        String product = "";
-        int quantity = 0;
         Scanner sc = new Scanner(System.in);
-        while(sc.hasNext()){
+        TreeMap<String, TreeMap<String, Integer>> customers = new TreeMap<>();
+
+        while(sc.hasNextLine()){
+
             String str = sc.nextLine();
             String[] arrOfStr = str.split(" ");
-            name = arrOfStr[0];
-            product = arrOfStr[1];
-            quantity = Integer.parseInt(arrOfStr[2]);
-            System.out.println(name);
-            System.out.println(product);
-            System.out.println(quantity);
-        }
 
-        TreeMap<String, Integer> products = new TreeMap<>();
-        // для каждого покупателя - новый объёкт
-        products.put(product, quantity);
-        if (products.containsKey(product)) {
-            int p_quantity = products.get(product);
-            p_quantity += quantity;
-            products.put(product, p_quantity);
-        }
+            String name = arrOfStr[0];
+            String product = arrOfStr[1];
+            int quantity = Integer.parseInt(arrOfStr[2]);
 
-        TreeMap<String, TreeMap<String, Integer>> customers = new TreeMap<>();
-        customers.put(name, new TreeMap<>());
-        TreeMap<String, Integer> petrov_products = customers.get(name); // получаем ссылку на словарь с покупками Петрова
-        System.out.println(petrov_products.size()); // напечатает 0
-        // добавляем покупки
-        petrov_products.put(product, quantity);
-        System.out.println(customers.get(name)); // напечатает 1
-        System.out.println(customers); // напечатает 1
+            TreeMap<String, Integer> products = new TreeMap<>();
+
+            if (customers.containsKey(name)){
+
+                products = customers.get(name);
+
+                if (products.containsKey(product)){
+
+                    int p_quantity = products.get(product);
+                    p_quantity += quantity;
+                    products.put(product, p_quantity);
+
+                }
+                else{
+                    products.put(product, quantity);
+                }
+            }
+            else{
+                products.put(product, quantity);
+            }
+
+            customers.put(name, products);
+            System.out.println(customers);
+
+        }
     }
-
 }
