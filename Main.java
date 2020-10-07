d9e25201a526640dfbd26c967d42f61006834d84
annngals <57696371+annngals@users.noreply.github.com>
2020-10-07 14:32:33 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,34 +5,39 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws IOException{
-        /*Большую часть времени потратила на ввод/вывод который плохо работает*/
-        /*У меня плохо с программированием*/
-        String arr[] = new String[6];
-        Scanner sc = new Scanner(new File("list.txt"));
-        for (int i = 0; i < 6; i++) {
-            arr[i] = sc.nextLine();
-            String[] str = arr[i].split(" ");
-            System.out.println(str);
+
+        String name = "";
+        String product = "";
+        int quantity = 0;
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            String str = sc.nextLine();
+            String[] arrOfStr = str.split(" ");
+            name = arrOfStr[0];
+            product = arrOfStr[1];
+            quantity = Integer.parseInt(arrOfStr[2]);
+            System.out.println(name);
+            System.out.println(product);
+            System.out.println(quantity);
         }
 
         TreeMap<String, Integer> products = new TreeMap<>();
         // для каждого покупателя - новый объёкт
-        products.put("paper", 10);
-        if (products.containsKey("paper")) {
-            int quantity = products.get("paper");
-            quantity += 5;
-            products.put("paper", quantity);
+        products.put(product, quantity);
+        if (products.containsKey(product)) {
+            int p_quantity = products.get(product);
+            p_quantity += quantity;
+            products.put(product, p_quantity);
         }
 
         TreeMap<String, TreeMap<String, Integer>> customers = new TreeMap<>();
-        customers.put("Petrov", new TreeMap<>());
-        TreeMap<String, Integer> petrov_products = customers.get("Petrov"); // получаем ссылку на словарь с покупками Петрова
+        customers.put(name, new TreeMap<>());
+        TreeMap<String, Integer> petrov_products = customers.get(name); // получаем ссылку на словарь с покупками Петрова
         System.out.println(petrov_products.size()); // напечатает 0
         // добавляем покупки
-        petrov_products.put("apple", 12);
-        // Вопрос: нужно ли после изменения состава покупок делать put?
-        customers.put("Petrov", petrov_products);
-        System.out.println(customers.get("Petrov").size()); // напечатает 1
+        petrov_products.put(product, quantity);
+        System.out.println(customers.get(name)); // напечатает 1
+        System.out.println(customers); // напечатает 1
     }
 
 }
